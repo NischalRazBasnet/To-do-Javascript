f68c1e971d7811fe32c8dd228463a816985c1861
NischalRazBasnet <nischalbasnet7848@gmail.com>
2024-01-08 16:40:21 +0545

    todo list html and css

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector("#addnew").oncick = function () {
+  if (document.querySelector('#newtask input').nodeValue.length == 0) {
+    alert("Task Cannot be Empty!!");
+  } else {
+    document.querySelector("#tasks").innerHTML += `<div class="task">
+        <span id="taskname">
+        ${document.querySelector("#newtask input").value}</span>
+        <button class="delete">
+        <i class="far fa-trash-alt"><i></button>
+        </div>`;
+  }
+};
