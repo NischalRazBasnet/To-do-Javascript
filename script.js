023bfe04df8a17b930c6321273c39827229f5bdf
NischalRazBasnet <nischalbasnet7848@gmail.com>
2024-01-08 19:17:46 +0545

    added edit and delete function along with completed button

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
-document.querySelector("#addnew").oncick = function () {
-  if (document.querySelector('#newtask input').value.length == 0) {
+document.querySelector("#addnew").onclick = function () {
+  var taskInput = document.querySelector("#newtask input");
+  if (taskInput.value.trim() === "") {
     alert("Task Cannot be Empty!!");
   } else {
-    document.querySelector("#tasks").innerHTML += `<div class="task">
-        <span id="taskname">
-        ${document.querySelector("#newtask input").value}</span>
-        <button class="delete">
-        <i class="far fa-trash-alt"><i></button>
-        </div>`;
+    var tasksContainer = document.querySelector("#tasks");
+
+    var newTask = document.createElement("div");
+    newTask.classList.add("task");
+
+    var taskName = document.createElement("span");
+    taskName.id = "taskname";
+    taskName.innerText = taskInput.value;
+
+    var completeButton = document.createElement("button");
+    completeButton.classList.add("completebtn");
+    completeButton.innerHTML = '<i class="fa-solid fa-thumbs-up"></i>';
+    completeButton.onclick = function () {
+      newTask.classList.toggle("completed");
+    };
+
+    var editButton = document.createElement("button");
+    editButton.classList.add("editbtn");
+    editButton.innerHTML = '<i class="fa-solid fa-pencil"></i>';
+    editButton.onclick = function () {
+      document.querySelector("#editTask").value = taskName.innerText;
+      document.querySelector("#editModal").style.display = "block";
+    };
+
+    var deleteButton = document.createElement("button");
+    deleteButton.classList.add("deletebtn");
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    deleteButton.onclick = function () {
+      newTask.remove();
+    };
+
+    newTask.appendChild(taskName);
+    newTask.appendChild(editButton);
+    newTask.appendChild(deleteButton);
+    newTask.appendChild(completeButton);
+
+    tasksContainer.appendChild(newTask);
+
+    taskInput.value = "";
   }
+};
+
+document.querySelector(".close").onclick = function () {
+  document.querySelector("#editModal").style.display = "none";
+};
+
+document.querySelector("#saveEdit").onclick = function () {
+  var editedTaskName = document.querySelector("#editTask").value;
+  document.querySelector("#taskname").innerText = editedTaskName;
+  document.querySelector("#editModal").style.display = "none";
 };
